dfb13cbd66d211b89e4f7970bc4414478aa7c7ce
Yurii Kosiv <41447717+yurdosii@users.noreply.github.com>
2021-01-10 13:08:00 +0200

    Update docker-compose.yml, requirements.txt. Add corsheaders

--- vlpi/settings.py
+++ vlpi/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "django_filters",
+    'corsheaders',
     # custom
     "modules",
     "tasks",
@@ -52,6 +53,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    'corsheaders.middleware.CorsMiddleware',
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
