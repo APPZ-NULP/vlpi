b504c084e639b40ef92e6fc8057e04fc8f1cf358
Yurii Kosiv <41447717+yurdosii@users.noreply.github.com>
2021-01-10 16:15:33 +0200

    feat: added /users/sign_in/ endpoint (#14)

--- users/views.py
+++ users/views.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import User
-from rest_framework import viewsets
+from rest_framework import status,viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from .serializers import UserSerializer
 
@@ -8,3 +10,12 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     lookup_field = "pk"
+
+    @action(detail=False, methods=["post"])
+    def sign_in(self, request):
+        username = request.data.get('username')
+        user = User.objects.filter(username=username).first()
+        if not user:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        serializer = self.get_serializer(user)
+        return Response(status=status.HTTP_200_OK, data=serializer.data)
